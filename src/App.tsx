e7ebd69f89ee55620fbba952958085d713fa56a3
minnan <minnan.liao@udngroup.com.tw>
2023-03-01 14:33:48 +0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Web3Modal from "web3modal";
+import { ethers } from "ethers";
+
+const web3Modal = new Web3Modal({
+  network: "goerli", 
+  providerOptions: {} 
+});
 
 function App() {
+  
+  useEffect(() => {
+    async function init() {
+      const instance = await web3Modal.connect();
+      const provider = new ethers.providers.Web3Provider(instance);
+      const signer = provider.getSigner();
+      const address = await signer.getAddress();
+      const balance = await provider.getBalance(address);
+      const ensAddress = await provider.lookupAddress(address);
+      
+      console.log(ethers);
+      console.log(instance);
+      console.log(address);
+      console.log(balance, `${ethers.utils.formatEther(balance)}ETH`);
+      console.log(ensAddress);
+      
+    };
+
+    init();
+  }, []);
+
+
   return (
     <div className="App">
       <header className="App-header">
